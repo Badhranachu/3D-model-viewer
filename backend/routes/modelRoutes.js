a63626eb1382f951ea2b58787d1647107599702b
Achu . <www.badhranks678@gmail.com>
2025-07-23 02:41:48 +0530

    Final CORS config with Vercel frontend p4

--- backend/routes/modelRoutes.js
+++ backend/routes/modelRoutes.js
@@ -9,6 +9,8 @@ const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/', upload.single('file'), uploadModel);
-router.get('/', getModels);
+router.get('/', getModels,(req, res) => {
+  res.json({ message: 'GET request successful' });
+});
 
 module.exports = router;
