bee29f040b223141938790463de6580d7ec4618d
Achu . <www.badhranks678@gmail.com>
2025-07-23 21:41:42 +0530

    update changes in all fetches p12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ import ModelList from './components/ModelList';
 const App = () => {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>3D Model Viewer</h1>
+      {/* <h1>3D Model Viewer</h1> */}
+      <h1>Love you kutta 😘</h1>
       <UploadForm />
       <ModelList />
     </div>
