0379aeb71dbba70b1c2ac0d230bbfebf4415cf3e
Achu . <www.badhranks678@gmail.com>
2025-07-23 20:12:43 +0530

    update changes in all fetches p5

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'https://3-d-model-viewer-sand.vercel.app',
+  origin: 'https://3-d-model-viewer-mu.vercel.app',
   credentials: true,
 }));
 
